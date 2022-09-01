099f2fc20760f747d504f960eb6cc3354e70d640
Ivan Zemlyanskiy <izemlyanskiy@pulsepoint.com>
2022-09-01 10:13:00 -0400

    upgrade spotless

--- src/main/java/com/pulsepoint/drawing/Drawing.java
+++ src/main/java/com/pulsepoint/drawing/Drawing.java
@@ -11,37 +11,37 @@ public class Drawing {
 
   public static void main(String[] args) throws IOException {
 
-    Scanner scanner = new Scanner(System.in);
+    try (Scanner scanner = new Scanner(System.in);
+        Writer writer = new PrintWriter(System.out, true);
+        PrintWriter error = new PrintWriter(System.err, true)) {
 
-    Writer writer = new PrintWriter(System.out, true);
-    PrintWriter error = new PrintWriter(System.err, true);
-    CanvasManager manager = new CanvasManager(new ConsoleCanvas());
+      CanvasManager manager = new CanvasManager(new ConsoleCanvas());
 
-    CommandParser parser = new CommandParser();
-    Command command = new NopCommand();
-    do {
-      writer.write(System.lineSeparator());
+      CommandParser parser = new CommandParser();
+      Command command = new NopCommand();
+      do {
+        writer.write(System.lineSeparator());
 
-      writer.write("Input command: ");
-      writer.flush();
-      String input = scanner.nextLine();
+        writer.write("Input command: ");
+        writer.flush();
+        error.flush();
+        String input = scanner.nextLine();
 
-      try {
-        command = parser.parseCommand(input);
-      } catch (IllegalCommandException | IllegalArgumentException e) {
-        System.err.println("Verify your input! Error message: " + e.getMessage());
-        continue;
-      }
+        try {
+          command = parser.parseCommand(input);
+        } catch (IllegalCommandException | IllegalArgumentException e) {
+          System.err.println("Verify your input! Error message: " + e.getMessage());
+          continue;
+        }
 
-      CommandResult commandResult = manager.applyCommand(command);
-      commandResult.printCommandResult(writer, error);
-      writer.append(System.lineSeparator());
+        CommandResult commandResult = manager.applyCommand(command);
+        commandResult.printCommandResult(writer, error);
+        writer.append(System.lineSeparator());
 
-      manager.show(writer);
-      writer.write(System.lineSeparator());
+        manager.show(writer);
+        writer.write(System.lineSeparator());
 
-    } while (!command.getClass().equals(Quit.class));
-
-    scanner.close();
+      } while (!command.getClass().equals(Quit.class));
+    }
   }
 }
